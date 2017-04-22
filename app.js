1557a9fa43ebfcfcdbc05ceec3318b05efedf6d9
Peter Žember <peter.zember@savero.net>
2017-04-23 01:16:55 +0200

    debug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(req, res) {
   var target = 'https://www.heroku.com';
   var host = 'www.heroku.com';
   var header = req.headers['authorization'];
-
+  var ver=false;
   if(header) {
     var token=header.split(/\s+/).pop()||'',
         auth=new Buffer(token, 'base64').toString(),
@@ -16,11 +16,12 @@ var server = http.createServer(function(req, res) {
         user=parts[0],
         pass=parts[1];
         if (process.env.USER === user  && process.env.PASS === pass) {
+          ver=true;
           target = process.env.TARGET;
           host = process.env.HOST;
         }
   }
-
+  console.log(ver, target, host)
   proxy.web(req, res, {
     target: target,
     agent  : https.globalAgent,
