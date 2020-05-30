9230575c66f3c51b35d60ee71eda33c3669f3c75
Shantanu Khandelwal <shantanu561993@gmail.com>
2020-05-30 16:36:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-var https     = require('https'),
-    http      = require('http'),
-    httpProxy = require('./lib/http-proxy'),
-    proxy = httpProxy.createProxyServer({});
+/*
+  web-socket-proxy.js: Example of proxying over HTTP and WebSockets.
+  Copyright (c) 2013 - 2016 Charlie Robbins, Jarrett Cruger & the Contributors.
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var util = require('util'),
+    http = require('http'),
+    colors = require('colors'),
+    httpProxy = require('./lib/http-proxy');
 
 
-var server = http.createServer(function(req, res) {
-  var target = 'https://www.heroku.com';
-  var host = 'www.heroku.com';
-  var header = req.headers['authorization'];
-  var ver=false;
-  if(header) {
-    var token=header.split(/\s+/).pop()||'',
-        auth=new Buffer(token, 'base64').toString(),
-        parts=auth.split(/:/),
-        user=parts[0],
-        pass=parts[1];
-        if (process.env.USER === user  && process.env.PASS === pass) {
-          ver=true;
-          target = process.env.TARGET;
-          host = process.env.HOST;
-        }
-  }
-  console.log(ver, target, host)
-  proxy.web(req, res, {
-    target: target,
-    agent  : https.globalAgent,
-    xfwd: false,
-    headers: {
-      host: host
-    }
-  });
-});
 
-server.listen(process.env.PORT || 8888);
+//
+// Create a proxy server with node-http-proxy
+//
+httpProxy.createServer({ target: 'ws://gpadvt.com:8080', ws: true }).listen(8014);
